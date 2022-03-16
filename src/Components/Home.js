c6810c69d0cd58b2a9b603a21d0c3c962e997654
rutiksangle3436 <rutiksangle3436@gmail.com>
2022-03-15 23:39:11 -0700

    Added Photo and Title

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div>
       <div class="flex items-center h-screen ">
-        <img src={Rutik} class="border-4 border-fly rounded-full  w-80 h-80 mx-20"> </img>
+        <img src={Rutik} class="border-4 border-fly rounded-full  w-80 h-80 mx-20" alt="Rutik"/>
         <div class="text-center PM text-fly w-50 mx-20">
           <blockquote>
             <p class="text-7xl font-large ">
